ae61c8bf38c050c629c8bac81010d9ba1958da0b
ddr997 <73550027+ddr997@users.noreply.github.com>
2022-05-15 16:33:03 +0200

    jaccard done, cosine in progressm

--- cw3/Cw3_239550_MaciejLukaszewicz.py
+++ cw3/Cw3_239550_MaciejLukaszewicz.py
@@ -1,7 +1,8 @@
-# Cw.2 EDWI (8.04.22) Maciej Lukaszewicz 239550, SRiPM Informatyka
+# Cw.2 EDWI (10.05.22) Maciej Lukaszewicz 239550, SRiPM Informatyka
 
 import requests, re, csv, string
 import nltk
+import numpy as np
 from collections import Counter
 from nltk.stem import PorterStemmer
 
@@ -12,6 +13,8 @@ class Crawler:
         self.initialURL = initialURL
         self.URLS = []
         self.invertedIndex = {}
+        self.decodedInvertedIndex = []
+        self.Ngrams = []
         try:
             headers = {
                 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36', }
@@ -26,8 +29,6 @@ class Crawler:
         regex = r'<(script|style).*>(.|\n)*?</(script|style)>|<[^>]*>'
         tagsRemoved = re.sub(regex, "", self.textWithHtmlTags)
         whitespacesRemoved = re.sub(r"\s{2,}", "\n", tagsRemoved)
-        # noSpaceSplitter = re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', whitespacesRemoved)
-        # filteredText = noSpaceSplitter
         filteredText = whitespacesRemoved
         return filteredText
 
@@ -67,15 +68,19 @@ class Crawler:
             listOfText.append([url, localInstance.removeTags()])
             listOfEmails.append([url, localInstance.getEmails()])
 
-    def getInvertedIndex(self, text, URL):
-        tokens = nltk.tokenize.word_tokenize(text)
+    def tokenize(self, textToFilter):
+        tokens = nltk.tokenize.word_tokenize(textToFilter)
         noPunctuation = [t for t in tokens if t not in string.punctuation] # filtr znakow
         pattern = re.compile(r"\b[^\d\W]+\b")
         noDigits = [t for t in noPunctuation if pattern.match(t)]
         ps = PorterStemmer()
-        stemming = [ps.stem(token) for token in noDigits]
-        invertedIndexDict = {key.lower():[URL] for key in stemming}
-        # print(invertedIndexDict)
+        stemmed = [ps.stem(token) for token in noDigits]
+        return stemmed
+
+    def getInvertedIndex(self, text, URL):
+        tokens = self.tokenize(text)
+        # print(tokens)
+        invertedIndexDict = {key.lower():[URL] for key in tokens}
         return invertedIndexDict
 
     def createInvertedIndex(self):
@@ -94,34 +99,98 @@ class Crawler:
             except:
                 print(f"({i})--Crawling of this site failed: {v}--")
                 continue
+
         self.invertedIndex = Builder
-        print("--Created inverted index:\n", self.invertedIndex)
+        self.decodedInvertedIndex = self.decodeIndex(Builder)
+        print("--Created inverted index (also created file):\n", self.decodedInvertedIndex)
+        self.writeToCsv([[i[0], " ".join(i[1])] for i in self.decodedInvertedIndex], "index")
         return Builder
 
+    def decodeIndex(self, toDecode):
+        return [[item, [self.URLS[index] for index in self.invertedIndex.get(item)]] for item in toDecode]
+
     def askForDocument(self, question: str):
-        tokens = nltk.tokenize.word_tokenize(question)
-        noPunctuation = [t for t in tokens if t not in string.punctuation] # filtr znakow
-        pattern = re.compile(r"\b[^\d\W]+\b")
-        noDigits = [t.lower() for t in noPunctuation if pattern.match(t)]
-        ps = PorterStemmer()
-        question = [ps.stem(token) for token in noDigits]
-        print("Zadane pytanie: ", " ".join(question))
-        links = []
-        for word in question:
+        tokenizedQuestion = self.tokenize(question)
+        print("Tokeny pytania: ", tokenizedQuestion)
+        linksFound = []
+        for word in tokenizedQuestion:
             try:
-                links.extend(self.invertedIndex[word])
+                linksFound.extend(self.invertedIndex[word])
             except:
                 continue
-        counter = Counter(links)
-        found = counter.most_common(8)
-        print("Dokumenty pasujace do zapytania: ", found)
-        for i in found:
-            print(self.URLS[ i[0] ])
+        counter = Counter(linksFound)
+        mostCommon = counter.most_common(8)
+        print("Dokumenty pasujace do zapytania: ", mostCommon)
+        for linkIndex in mostCommon:
+            print(f"Count:({linkIndex[1]}) ",self.URLS[linkIndex[0]])
+
+    def createNGram(self, tokens: list, n: int):
+        length = len(tokens)
+        Ngrams = []
+        for i in range(0, length-n+1):
+            Ngrams.append(
+                " ".join(
+                    tokens[i:i+n]
+                )
+            )
+        return Ngrams
+
+    def createNgrams(self, n: int):
+        urlsToVisit = self.getUrls()
+        # Builder = [self.createNGram(
+        #     self.tokenize(
+        #         self.removeTagsFromHtml()
+        #     )
+        # , n)]
+        Builder = []
+
+        for i, v in enumerate(urlsToVisit):
+            try:
+                localCrawl = Crawler(v)
+                print(f"({i})Visiting and creating Ngram: {v}")
+                Builder.append(
+                    localCrawl.createNGram(
+                        self.tokenize(
+                            localCrawl.removeTagsFromHtml()
+                        )
+                    , n)
+                )
+            except:
+                print(f"({i})--  Ngrams creation failed: {v}  --")
+                Builder.append(["error"])
+                continue
+        self.Ngrams = Builder
+        return Builder
+
+    def createJaccardIndexRanking(self):
+        length = len(self.URLS)
+        jaccard = np.zeros((length, length))
+        for i, A in enumerate(self.Ngrams):
+            for j, B in enumerate(self.Ngrams):
+                sameElements = list(set(A).intersection(B))
+                jaccard[i, j] = len(sameElements)/(len(set(A))+len(set(B))-len(sameElements))
+        return np.round(jaccard, 3)
+
+    def createCosineDistanceRanking(self):
+        bagOfWords = list(set(
+            [j for i in self.Ngrams for j in i]
+        ))
+        vectors = []
+        for siteGrams in self.Ngrams:
+            emptyVector = [0] * len(bagOfWords)
+            for i, word in enumerate(bagOfWords):
+                if word in siteGrams:
+                    emptyVector[i] = 1
+            vectors.append(emptyVector)
+        return vectors
 
 
 if __name__ == "__main__":
     URL = input("Enter the URL (press Enter for default): ") or "https://en.wikipedia.org/wiki/Wykop.pl"
     crawler = Crawler(URL)
-    crawler.createInvertedIndex()
-    question = input("Zadaj pytanie: ") or "wykop.pl Janusz Krzysztof AMA"
-    crawler.askForDocument(question)
+    # crawler.createInvertedIndex()
+    # question = input("Zadaj pytanie: ") or "wykop.pl Janusz Krzysztof AMA"
+    # crawler.askForDocument(question)
+    x = crawler.createNgrams(2)
+    print(crawler.createJaccardIndexRanking())
+    print(crawler.createCosineDistanceRanking())
